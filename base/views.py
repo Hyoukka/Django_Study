5e5574ab1c55f87d8f4a37dffe4ad4b526949056
Cassius Vinicius <cassiusoliveira10@gmail.com>
2023-02-27 11:18:52 -0400

    Mobile Responsiveness

--- base/views.py
+++ base/views.py
@@ -64,7 +64,7 @@ def home(request):
         Q(name__icontains=q) |
         Q(description__icontains=q)
         )
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
@@ -175,7 +175,10 @@ def updateUser(request):
     return render(request, 'base/update-user.html', {'form':form})   
 
 def topicsPage(request):
-    pass
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html', {'topics': topics})
 
 def activityPage(request):
-    pass
+    room_messages = Message.objects.all()
+    return render(request, 'base/activity.html', {'room_messages': room_messages})
